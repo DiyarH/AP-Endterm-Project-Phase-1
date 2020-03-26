7e1e3ef820846b9142e07d933b72a217bc18fac7
Diyar Hamedi <diyarhamedi81@gmail.com>
2020-03-26 23:20:59 +0430

    all completed

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,58 @@ namespace AP_Endterm_Project_Phase_1
         public MainWindow()
         {
             InitializeComponent();
+            DateTime today = DateTime.Today;
+            for (int i = 1901; i <= 2050; ++i)
+            {
+                year.Items.Add(i);
+            }
+            for (int i = 1; i <= 12; ++i)
+            {
+                month.Items.Add(i);
+            }
+            for (int i = 1; i <= 31; ++i)
+            {
+                day.Items.Add(i);
+            }
+            year.SelectedItem = today.Year;
+            month.SelectedItem = today.Month;
+            day.SelectedItem = today.Day;
+            nextPage.IsEnabled = tab.SelectedIndex < (tab.Items.Count - 1);
+            previousPage.IsEnabled = tab.SelectedIndex > 0;
+        }
+
+        private void nextPage_Click(object sender, RoutedEventArgs e)
+        {
+            tab.SelectedIndex++;
+        }
+
+        private void previousPage_Click(object sender, RoutedEventArgs e)
+        {
+            tab.SelectedIndex--;
+        }
+
+        private void tab_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (nextPage != null)
+            {
+                nextPage.IsEnabled = tab.SelectedIndex < (tab.Items.Count - 1);
+            }
+            if (previousPage != null)
+            {
+                previousPage.IsEnabled = tab.SelectedIndex > 0;
+            }
+        }
+
+        private void colorValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (colorValue != null)
+            {
+                colorValue.Text = ((int)red.Value).ToString("X").PadLeft(2, '0') + ((int)green.Value).ToString("X").PadLeft(2, '0') + ((int)blue.Value).ToString("X").PadLeft(2, '0');
+            }
+            if (shape != null)
+            {
+                shape.Fill = new SolidColorBrush(Color.FromRgb((byte)red.Value, (byte)green.Value, (byte)blue.Value));
+            }
         }
     }
 }
